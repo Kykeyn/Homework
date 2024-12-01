e58885464671c0c05625c24b48c65257bce11e56
Kykeyn <Kykeyn@gmail.com>
2024-12-01 23:27:53 +0300

    module_9_2.py

--- module_9_2.py
+++ module_9_2.py
@@ -2,17 +2,10 @@ first_strings = ["Elon", "Musk", "Programmer", "Monitors", "Variable"]
 second_strings = ["Task", "Git", "Comprehension", "Java", "Computer", "Assembler"]
 
 first_result = [len(i) for i in first_strings if len(i) >= 5]
-second_result = []
-third_result = {}
-
-for i in first_strings:
-    for j in second_strings:
-        if len(i) == len(j):
-            second_result.append((i, j))
-
-for i in first_strings + second_strings:
-    if len(i) % 2 == 0:
-        third_result[i] = len(i)
+second_result = [
+    (i, j) for i in first_strings for j in second_strings if len(i) == len(j)
+]
+third_result = {i: len(i) for i in first_strings + second_strings if len(i) % 2 == 0}
 
 
 print(first_result)
